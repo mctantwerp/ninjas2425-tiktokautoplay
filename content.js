eddfb829d7f304900a3280373ca9f010ac1c04c2
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-12-23 07:17:59 +0100

    all tiktok videos now autoplay

--- content.js
+++ content.js
@@ -1,54 +1,35 @@
-let videoElements = null;
-let video = null;
+let video;
 let index = 0;
+let scrollContainer = document.querySelector('.css-1xzh9re-DivColumnListContainer.e108hwin0');
+console.log('hi from content.js');
 
+document.addEventListener('visibilitychange', async function () {
+    video = document.querySelectorAll('video')[index];
+    await delay(50);
+    if (video.paused) {
+        video.play();
+    }
 
-document.addEventListener('DOMContentLoaded', function () {
-    videoElements = document.querySelectorAll('video');;
-    video = videoElements[index];
-    console.log('Video elements found');
-    if (videoElements.length > 0) {
-        video = videoElements[index];
-    } else {
-        console.log('No video elements found');
+});
+
+scrollContainer.addEventListener('scroll', async function () {
+    if (video && !video.paused) {
+        video.pause();
+    }
+
+    if (index < 1) {
+        index++;
+    }
+    await delay(1000);
+    video = document.querySelectorAll('video')[index];
+    console.log(index);
+
+    if (video.paused) {
+        video.play();
     }
 });
 
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-document.addEventListener('visibilitychange', function () {
-    videoElements = document.querySelectorAll('video');
-    video = videoElements[index];
-    console.log('Video elements found');
-    console.log(videoElements);
-    console.log(video);
-    console.log(index);
-    video.play();
-});
-document.addEventListener('touchend', function () {
-    videoElements = document.querySelectorAll('video');
-    console.log(index);
-    if (index < 2) {
-        index = Math.min(index + 1, videoElements.length - 1);
-    }
-    console.log(index);
-    video = videoElements[index];
-    console.log('Video elements found');
-    console.log(videoElements);
-    console.log("Scrolling");
-    console.log(video);
-    console.log(index);
-});
-document.addEventListener('scroll', function () {
-    videoElements = document.querySelectorAll('video');
-    console.log(index);
-    if (index < 2) {
-        index = Math.min(index + 1, videoElements.length - 1);
-    }
-    console.log(index);
-    video = videoElements[index];
-    console.log('Video elements found');
-    console.log(videoElements);
-    console.log("Scrolling");
-    console.log(video);
-    console.log(index);
-});
