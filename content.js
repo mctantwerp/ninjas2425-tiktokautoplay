343353226c5d7f029a8776b960dcdace3023d529
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-12-23 15:56:17 +0100

    checkbox now works in real-time

--- content.js
+++ content.js
@@ -1,35 +1,67 @@
 let video;
 let index = 0;
 let scrollContainer = document.querySelector('.css-1xzh9re-DivColumnListContainer.e108hwin0');
+let extensionEnabled;
+chrome.storage.sync.get(['autoplayEnabled'], (result) => {
+    extensionEnabled = result.autoplayEnabled || false;
+});
 console.log('hi from content.js');
 
 document.addEventListener('visibilitychange', async function () {
-    video = document.querySelectorAll('video')[index];
-    await delay(50);
-    if (video.paused) {
-        video.play();
+    if (extensionEnabled) {
+        video = document.querySelectorAll('video')[index];
+        await delay(50);
+        if (video.paused) {
+            video.play();
+        }
     }
 
 });
 
 scrollContainer.addEventListener('scroll', async function () {
-    if (video && !video.paused) {
-        video.pause();
-    }
+    if (extensionEnabled) {
+        if (video && !video.paused) {
+            video.pause();
+        }
 
-    if (index < 1) {
-        index++;
-    }
-    await delay(1000);
-    video = document.querySelectorAll('video')[index];
-    console.log(index);
+        if (index < 1) {
+            index++;
+        }
+        await delay(1000);
+        video = document.querySelectorAll('video')[index];
+        console.log(index);
 
-    if (video.paused) {
-        video.play();
+        if (video.paused) {
+            video.play();
+        }
+    }
+    else {
+        if (index < 1) {
+            index++;
+        }
     }
 });
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+// Listen for changes to chrome.storage
+chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName === 'sync' && changes.autoplayEnabled) {
+        console.log("Autoplay Enabled changed:", changes.autoplayEnabled.newValue);
+
+        // Perform actions based on the new value of autoplayEnabled
+        if (changes.autoplayEnabled.newValue) {
+            // Enable autoplay
+            console.log("Autoplay is now enabled");
+            extensionEnabled = true;
+        } else {
+            // Disable autoplay
+            console.log("Autoplay is now disabled");
+            extensionEnabled = false;
+        }
+    }
+});
+
 
