5b5d45a431c650f079cd09b4b74f5598e166692e
gillesserrien <146174695+gillesserrien@users.noreply.github.com>
2024-12-23 06:32:44 +0100

    temp commit

--- content.js
+++ content.js
@@ -1,14 +1,54 @@
-console.log("Hello from content.js");
-let video = document.querySelector('video');
-document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState === 'hidden') {
-        // Find the video element
-        video = document.querySelector('video');
-        console.log('VIDEO LOG');
-        console.log(video);
-        if (video && video.paused) {
-            // Resume the video playback
-            video.play();
-        }
+let videoElements = null;
+let video = null;
+let index = 0;
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    videoElements = document.querySelectorAll('video');;
+    video = videoElements[index];
+    console.log('Video elements found');
+    if (videoElements.length > 0) {
+        video = videoElements[index];
+    } else {
+        console.log('No video elements found');
     }
+});
+
+
+document.addEventListener('visibilitychange', function () {
+    videoElements = document.querySelectorAll('video');
+    video = videoElements[index];
+    console.log('Video elements found');
+    console.log(videoElements);
+    console.log(video);
+    console.log(index);
+    video.play();
+});
+document.addEventListener('touchend', function () {
+    videoElements = document.querySelectorAll('video');
+    console.log(index);
+    if (index < 2) {
+        index = Math.min(index + 1, videoElements.length - 1);
+    }
+    console.log(index);
+    video = videoElements[index];
+    console.log('Video elements found');
+    console.log(videoElements);
+    console.log("Scrolling");
+    console.log(video);
+    console.log(index);
+});
+document.addEventListener('scroll', function () {
+    videoElements = document.querySelectorAll('video');
+    console.log(index);
+    if (index < 2) {
+        index = Math.min(index + 1, videoElements.length - 1);
+    }
+    console.log(index);
+    video = videoElements[index];
+    console.log('Video elements found');
+    console.log(videoElements);
+    console.log("Scrolling");
+    console.log(video);
+    console.log(index);
 });
